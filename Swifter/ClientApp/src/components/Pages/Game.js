2c8cd2834ed0f8e56d8bc5214f75bbd6b660c7da
Karlis Oglins <karlis.oglins@gmail.com>
2022-11-16 22:16:34 +0200

    Created a simple game grid and logic to create a basic game

--- Swifter/ClientApp/src/components/Pages/Game.js
+++ Swifter/ClientApp/src/components/Pages/Game.js
@@ -1,11 +1,45 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useState } from 'react';
+import GameManager from '../GameManagement/GameManager';
 
 const Game = () => {
+    const [gameScore, setGameScore] = useState(0)
+    const [moveCount, setMoveCount] = useState(0)
+    const [timeElapsed, setTimeElapsed] = useState(0)
+    const [selectedGameMode, setSelectedGameMode] = useState({})
+    const [isGamePaused, setIsGamePaused] = useState(true)
+    const [draggable, setDraggable] = useState(true)
+    const [hasGameStarted, setHasGameStarted] = useState(true) // needs to be false
+    const [hasGameEnded, setHasGameEnded] = useState(false)
+
+    const height = 8
+    const width = 8
+
+    useEffect(() => {
+        setSelectedGameMode('standard')
+    }, [])
+
     return (
         <div>
 
             <h1>Game page</h1>
             <p>This is the game page.</p>
+
+
+            <GameManager
+                height={height}
+                width={width}
+                selectedGameMode={selectedGameMode}
+                moveCount={moveCount}
+                setMoveCount={setMoveCount}
+                timeElapsed={timeElapsed}
+                draggable={draggable}
+                setDraggable={setDraggable}
+                isGamePaused={isGamePaused}
+                setIsGamePaused={setIsGamePaused}
+                setHasGameEnded={setHasGameEnded}
+                setGameScore={setGameScore}
+                hasGameEnded={hasGameEnded}
+            />
 
         </div>
     );
