6984ce1c004ebac7eab0c422391da4791aee15c2
Karlis Oglins <karlis.oglins@gmail.com>
2022-11-21 15:36:10 +0200

    Created a new project with model for game modes

---
+++ Swifter.Data/Models/GameMode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swifter.Data.Models
+{
+    public class GameMode
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string DisplayName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public bool HasRules { get; set; }
+        [Required]
+        public int? Height { get; set; }
+        [Required]
+        public int? Width { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public int? Duration { get; set; }
+    }
+}
